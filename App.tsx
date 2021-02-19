284366abc468894069498ceffbb1f73f9419b95d
Alexis Petroccelli <apet3001@7-11.com>
2021-02-19 01:10:34 -0300

    [main] get app running with flatlist example

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import {HomeScreen} from "./src/HomeScreen/HomeScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <Text>Open up App.tsx to start working on your app!</Text> */}
+      {/* <StatusBar style="auto" /> */}
+      <HomeScreen />
     </View>
   );
 }
@@ -14,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- README.md
+++ README.md
@@ -10,3 +10,13 @@ You can follow the [React Native environment set up](https://reactnative.dev/doc
 ## Project structure
 The structure for this project will be organized by feature. This will ensure the files for the same component to be all together.
 (For more information read the [Khoa Pham article](https://www.freecodecamp.org/news/how-to-structure-your-project-and-manage-static-resources-in-react-native-6f4cfc947d92/) on FreeCodeCamp)
+
+## Errors
+
+node_modules\expo\AppEntry.js: [BABEL] C:\Users\AlexisPetroccelli\Documents\personal\projects\gatito-app\node_modules\expo\AppEntry.js: Unknown option: .name. Check out https://babeljs.io/docs/en/babel-core/#options for more information about options.
+
+https://stackoverflow.com/questions/65158092/react-native-app-error-unknown-option-name
+
+
+typescript modules not loading
+https://github.com/tleunen/babel-plugin-module-resolver/issues/387#issuecomment-631555493
