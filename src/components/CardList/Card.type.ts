c6b3fa8a40bf152d932d246b8f33a112ed512071
Alexis Petroccelli <apet3001@7-11.com>
2021-03-03 00:28:35 -0300

    add cat API endpoint call

---
+++ src/components/CardList/Card.type.ts
@@ -0,0 +1,7 @@
+export interface ICard {
+    "breeds": object[],
+    "id": string,
+    "url": string,
+    "width": number,
+    "height": number
+  }
--- src/library/networking/API.tsx
+++ src/library/networking/API.tsx
@@ -1,13 +1,41 @@
-import axios from 'axios';
+import axios from "axios";
 // @ts-ignore
-import {API_KEY} from 'react-native-dotenv';
+import { API_KEY } from "@env";
+// import { API_KEY } from "react-native-dotenv";
+import { ICard } from "components/CardList/Card.type";
 
+// const axiosInstance = axios.create({
+//   baseURL: "https://api.thecatapi.com",
+//   headers: {
+//     common: {
+//       "x-api-key": API_KEY,
+//     }
+//   }
+// });
 
+axios.interceptors.response.use((res: any) => {
+  res.data.map((element: any) => {
+    const card: ICard = {
+      breeds: element.breeds,
+      id: element.id,
+      url: element.url,
+      width: element.width,
+      height: element.height,
+    };
+
+    return card;
+  });
+
+  return res;
+});
 export const getCatPics = async () => {
-  let res = await axios.get('https://api.thecatapi.com/v1/images/search', {
-    headers: {
-      'x-api-key': API_KEY
-    }});
+  let res = await axios.get(
+    "https://api.thecatapi.com/v1/images/search?limit=25",
+    {
+      headers: {
+        "x-api-key": API_KEY,
+      },
+    }
+  );
   return res.data;
-}
-
+};
