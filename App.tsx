52bd315e9ae38e589482777e819ae099eca8e716
Alexis Petroccelli <apet3001@7-11.com>
2021-03-08 23:45:37 -0300

    add fullscreen image

--- App.tsx
+++ App.tsx
@@ -1,12 +1,9 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 import { HomeScreen } from "./src/screens/HomeScreen/HomeScreen";
-import { CardList } from "./src/components/CardList/CardList";
 
 export default function App() {
-
   return (
     <View style={styles.container}>
       <HomeScreen />
@@ -17,8 +14,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // backgroundColor: "#fff",
-    // alignItems: "center",
-    // justifyContent: "center",
   },
 });
--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,142 +1,14 @@
-import React, { useEffect, useState, useRef  } from "react";
-import {
-  StatusBar,
-  Image,
-  FlatList,
-  Dimensions,
-  Animated,
-  Text,
-  View,
-  StyleSheet,
-  SafeAreaView,
-  Button,
-} from "react-native";
-const { width } = Dimensions.get("screen");
-import { EvilIcons } from "@expo/vector-icons";
-import {
-  FlingGestureHandler,
-  Directions,
-  State,
-} from "react-native-gesture-handler";
+import React, { useState, useRef } from "react";
 
 import { CardList } from "../../components/CardList/CardList";
-import { getCatPics } from "library/networking";
 
-
-const DIMENSIONS = Dimensions.get("window");
-
-export const HomeScreen = (props: any) => {
+export const HomeScreen = () => {
   const [catPics, setCatPics] = useState([]);
-  const list:any = useRef();
-
-  useEffect(() => {
-    (async () => {
-      const response = await getCatPics();
-      setCatPics(response);
-    })();
-  }, []);
-
-
-  const picBack = () => {
-    console.log("picBack", list.current.moveLeft());
-  };
-  const picFw = () => {
-    console.log("picFw", list.current.moveRight());
-  };
+  const list: any = useRef();
 
   return (
     <>
-      <CardList
-        cardList={catPics}
-        style={{
-          width: DIMENSIONS.width,
-        }}
-        ref={list}
-      />
-      <View
-        style={{
-          justifyContent: "space-evenly",
-          flexDirection: "row",
-        }}
-      >
-        <Button onPress={picBack} title="<" color="#841584" />
-        <Button
-          onPress={picFw}
-          title=">"
-          color="#841584"
-          accessibilityLabel="Learn more about this purple button"
-        />
-      </View>
+      <CardList cardList={catPics} onTouch={() => {}} ref={list} />
     </>
   );
 };
-
-const Styles = StyleSheet.create({
-  card: {
-    height: 500,
-    width: 350,
-    borderWidth: 1,
-    borderColor: "#A9A9A9",
-    borderRadius: 8,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#FFF",
-    overflow: "hidden",
-  },
-  cardTop: {
-    position: "absolute",
-    left: 0,
-    top: 0,
-    height: 200,
-    width: 350,
-    backgroundColor: "#D3D3D3",
-  },
-  cardImage: {
-    position: "absolute",
-    left: 85,
-    top: 110,
-    width: 180,
-    height: 180,
-    borderRadius: 90,
-    borderColor: "#FFF",
-    borderWidth: 4,
-    backgroundColor: "#1E90FF",
-  },
-  cardImageBorder: {
-    position: "absolute",
-    left: 83.5,
-    top: 108.5,
-    width: 183,
-    height: 183,
-    borderRadius: 91.5,
-    backgroundColor: "#A9A9A9",
-  },
-  cardText: {
-    position: "absolute",
-    left: 0,
-    top: 300,
-    width: 350,
-    alignItems: "center",
-    padding: 20,
-  },
-  cardTextMain: {
-    textAlign: "left",
-    fontSize: 25,
-    color: "#696969",
-    backgroundColor: "transparent",
-    paddingBottom: 10,
-  },
-  cardTextSecondary: {
-    textAlign: "left",
-    fontSize: 18,
-    color: "grey",
-    backgroundColor: "transparent",
-  },
-  cardTextTerciary: {
-    textAlign: "left",
-    fontSize: 18,
-    color: "#696969",
-    backgroundColor: "transparent",
-    paddingTop: 10,
-  },
-});
